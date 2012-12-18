20e789d498e69fa5cb7497055d7b499f20273ae3
Jayanth Chennamangalam <jayanthc@gmail.com>
2012-12-18 17:38:11 -0500

    Added e-mail address.

---
+++ js/email.js
@@ -0,0 +1,13 @@
+/*
+ * email.js
+ * Email address protection
+ *
+ * Created by Jayanth Chennamangalam
+ */
+
+var userid = "jayanth";
+var domain = "jayanthchennamangalam.com";
+
+document.write("<a href=\"mailto:" + userid + "@" + domain + "\">"
+               + userid + "@" + domain + "</a>");
+
