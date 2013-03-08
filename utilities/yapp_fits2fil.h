ae0f686e32cb1f19a37c12ca5750869971a07c53
Jayanth Chennamangalam <jayanthc@gmail.com>
2013-03-07 16:20:02 -0800

    Added utilities/yapp_fits2fil.h.

---
+++ utilities/yapp_fits2fil.h
@@ -0,0 +1,17 @@
+/**
+ * @file yapp_fits2fil.h
+ * Header file for yapp_fits2fil
+ *
+ * @author Jayanth Chennamangalam
+ * @date 2013.03.06
+ */
+
+#ifndef __YAPP_FITS2FIL_H__
+#define __YAPP_FITS2FIL_H__
+
+#define YAPP_MAX_NPOL           2
+
+int YAPP_WritePolSelection(int iNumBits, long int lLen, char cToSum);
+
+#endif  /* __YAPP_FITS2FIL_H__ */
+
