f68c8a2cc4346963f5ffd5bacae5475710b9200c
Jayanth Chennamangalam <jayanthc@gmail.com>
2014-12-12 10:31:06 +0000

    Bugfix related to reading files with nbits >= 8.

--- src/yapp_common.c
+++ src/yapp_common.c
@@ -1708,7 +1708,7 @@ int YAPP_ReadData(FILE *pFData,
                   int iTotSampsPerBlock)
 {
     static char cIsFirst = YAPP_TRUE;
-    static char *pcBuf = NULL;
+    static unsigned char *pcBuf = NULL;
     int iReadItems = 0;
     int i = 0;
 
@@ -1724,7 +1724,7 @@ int YAPP_ReadData(FILE *pFData,
         /* allocate memory for the byte buffer, based on the total number of
            samples per block (= number of channels * number of time samples per
            block) */
-        pcBuf = (char *) YAPP_Malloc((int) (iTotSampsPerBlock * fSampSize),
+        pcBuf = (unsigned char *) YAPP_Malloc((int) (iTotSampsPerBlock * fSampSize),
                                      sizeof(char),
                                      YAPP_FALSE);
         if (NULL == pcBuf)
@@ -1739,7 +1739,7 @@ int YAPP_ReadData(FILE *pFData,
 
     /* read data into the byte buffer */
     iReadItems = fread(pcBuf,
-                       sizeof(char),
+                       sizeof(unsigned char),
                        (int) (iTotSampsPerBlock * fSampSize),
                        pFData);
     if (ferror(pFData))
