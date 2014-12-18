jayanthc/yapp

commit b3675d3dbc4125768f55e635bba34eed4bc06a79

Minor bug fixes.

src/yapp_dedisperse.c
@@ -86,11 +86,9 @@ int main(int argc, char *argv[])
     float fSampSize = 0.0;      /* number of bits that make a sample */
     int iTotSampsPerBlock = 0;  /* iNumChans * iBlockSize */
     int iDataSizePerBlock = 0;  /* fSampSize * iNumChans * iBlockSize */
-    int iNumGoodChans = 0;
     int iEffcNumGoodChans = 0;
     float fStatBW = 0.0;
     float fNoiseRMS = 0.0;
-    double dNumSigmas = 0.0;
     double dTNextBF = 0.0;
     double dTSamp = 0.0;        /* holds sampling time in ms */
     double dTSampInSec = 0.0;   /* holds sampling time in s */
@@ -368,7 +366,6 @@ int main(int argc, char *argv[])
     dTSampInSec = stYUM.dTSamp / 1e3;
     fChanBW = stYUM.fChanBW;
     iTimeSamps = stYUM.iTimeSamps; 
-    iNumGoodChans = stYUM.iNumGoodChans;
     fSampSize = stYUM.fSampSize;
     lDataSizeTotal = stYUM.lDataSizeTotal;
     pfTimeSectGain = stYUM.pfBFGain;    /* for .spec */
@@ -567,19 +564,6 @@ int main(int argc, char *argv[])
                   iNumReads,
                   iBlockSize);
 
-    /* calculate the threshold */
-    dNumSigmas = YAPP_CalcThresholdInSigmas(iTimeSampsToProc);
-    if ((double) YAPP_RET_ERROR == dNumSigmas)
-    {
-        (void) fprintf(stderr, "ERROR: Threshold calculation failed!\n");
-        YAPP_CleanUp();
-        return YAPP_RET_ERROR;
-    }
-    fStatBW = iNumGoodChans * fChanBW;  /* in MHz */
-    (void) printf("Usable bandwidth                  : %g MHz\n", fStatBW);
-    fNoiseRMS = 1.0 / sqrt(fStatBW * dTSamp * 1e3);
-    (void) printf("Expected noise RMS                : %g\n", fNoiseRMS);
-
     /* allocate memory for the time sample goodness flag array */
     g_pcIsTimeGood = (char *) YAPP_Malloc((size_t) iTimeSampsToProc,
                                           sizeof(char),
@@ -595,27 +579,38 @@ int main(int argc, char *argv[])
     /* set all elements to 'YAPP_TRUE' */
     (void) memset(g_pcIsTimeGood, YAPP_TRUE, iTimeSampsToProc);
 
+    /* add header for .fil or .tim file format */
+    if ((YAPP_FORMAT_FIL == iOutputFormat)
+        || (YAPP_FORMAT_DTS_TIM == iOutputFormat))
+    {
+        /* populate the YUM structure */
+        (void) memcpy(&stYUMOut, &stYUM, sizeof(YUM_t));
+    }
+
     /* kludge: simulate sub-band dedispersion by marking other channels as bad
        so that they don't go into the summation */
-    /* NOTE: this will not work with data in which there are actual bad
-       channels */
     if (iNumSubBands > 0)
     {
-        iChansPerSubBand = (int) ((float) iNumChans / iNumSubBands);
+        /* compute the number of channels per sub-band. iNumChans is a multiple
+           of iNumSubBands */
+        iChansPerSubBand = iNumChans / iNumSubBands;
         iStartChan = iSubBand * iChansPerSubBand;
         iEndChan = (iSubBand + 1) * iChansPerSubBand;
+        stYUMOut.iNumGoodChans = iChansPerSubBand;
         for (i = 0; i < iNumChans; ++i)
         {
-            if ((i >= iStartChan) && (i < iEndChan))
-            {
-                stYUM.pcIsChanGood[i] = YAPP_TRUE;
-            }
-            else
+            if (!((i >= iStartChan) && (i < iEndChan)))
             {
                 stYUM.pcIsChanGood[i] = YAPP_FALSE;
+                --stYUMOut.iNumGoodChans;
             }
         }
     }
+
+    fStatBW = stYUMOut.iNumGoodChans * fChanBW;  /* in MHz */
+    (void) printf("Usable bandwidth                  : %g MHz\n", fStatBW);
+    fNoiseRMS = 1.0 / sqrt(fStatBW * dTSamp * 1e3);
+    (void) printf("Expected noise RMS                : %g\n", fNoiseRMS);
 
     /* open the data file for reading */
     g_pFData = fopen(pcFileSpec, "r");
@@ -898,12 +893,10 @@ int main(int argc, char *argv[])
         (void) strcat(acFileDedisp, EXT_FIL);
     }
 
-    /* add header for .fil or .tim file format */
+    /* update header for .fil or .tim file format */
     if ((YAPP_FORMAT_FIL == iOutputFormat)
         || (YAPP_FORMAT_DTS_TIM == iOutputFormat))
     {
-        /* populate the YUM structure */
-        (void) memcpy(&stYUMOut, &stYUM, sizeof(YUM_t)); 
         stYUMOut.iNumBits = YAPP_SAMPSIZE_32;
         /* enter the start time corrected for dispersion */
         stYUMOut.dTStart = stYUM.dTStart - (fStartOffset / 86400);
@@ -919,7 +912,6 @@ int main(int argc, char *argv[])
         if (iNumSubBands > 0)
         {
             stYUMOut.iNumChans = iChansPerSubBand;
-            stYUMOut.iNumGoodChans = iChansPerSubBand;
             stYUMOut.fBW = stYUMOut.iNumChans * stYUMOut.fChanBW;
             stYUMOut.fFMin = stYUM.fFMin
                              + (iStartChan * stYUMOut.fChanBW);

tags
@@ -1,19 +1,11 @@
 CAND_INFO	src/yapp.h	/^} CAND_INFO;$/
 CMap_t	src/colourmap.h	/^} CMap_t;$/
 GetColourMapFromName	src/colourmap.c	/^int GetColourMapFromName(char *pcCMapName)$/
-Myapp_dat2tim	utilities/yapp_dat2tim.c	/^int main(int argc, char *argv[])$/
-Myapp_fits2fil	utilities/yapp_fits2fil.c	/^int main(int argc, char *argv[])$/
 Myapp_pulsarsnd	src/yapp_pulsarsnd.c	/^int main(int argc, char *argv[])$/
-Myapp_tim2dat	utilities/yapp_tim2dat.c	/^int main(int argc, char *argv[])$/
 PFB_DATA	src/yapp_ft.h	/^} PFB_DATA;$/
 Plot2D	src/colourmap.c	/^void Plot2D(float* pfBuf, float fDataMin, float fD/
 PrintUsage	src/yapp_pulsarsnd.c	/^void PrintUsage(const char *pcProgName)$/
 SetColourMap	src/colourmap.c	/^int SetColourMap(int iCMap, float fColMin, float f/
-YAPP_CopyData	utilities/yapp_dat2tim.c	/^int YAPP_CopyData(char *pcFileData, FILE *pFTim)$/
 YAPP_SIGPROC_HEADER	src/yapp_sigproc.h	/^} YAPP_SIGPROC_HEADER;$/
-YAPP_WritePolSelection	utilities/yapp_fits2fil.c	/^int YAPP_WritePolSelection(int iNumBits, long int /
 YUM_t	src/yapp.h	/^} YUM_t;$/
-f	src/yapp_common.o	/^/
-read	src/yapp_ft.o	/^/
 tagFileFormats	src/yapp.h	/^enum tagFileFormats$/
-x	src/yapp_add.o	/^Οϊνώ/

src/yapp_add.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[])
     /* read metadata */
     /* NOTE: it is assumed that all files correspond to a single observation.
              the only information taken from all files except the last one is
-             the highest frequency. general metadata is read from the last
+             the highest frequency. common metadata is read from the last
              file */
     /* allocate memory for the frequency array */
     pafMaxFreq = (float *) YAPP_Malloc((size_t) iNumBands,

commit ce8d89ad276bad53489aeecd659d8e2e9c838856

Fixed subband dedispersion bug introduced in b3675d3.

src/yapp_dedisperse.c
@@ -651,7 +651,6 @@ int main(int argc, char *argv[])
             if (!((i >= iStartChan) && (i < iEndChan)))
             {
                 stYUM.pcIsChanGood[i] = YAPP_FALSE;
-                --stYUMOut.iNumGoodChans;
             }
         }
     }

src/yapp_makever.c
@@ -24,7 +24,7 @@
 /* TODO: read the path as argument */
 #define FILE_VERSRC         "src/yapp_version.c"
 #define VAR_VER             "*g_pcVersion"
-#define VER_BUILD_PREFIX    "YAPP-DEV-3.5.5-beta"
+#define VER_BUILD_PREFIX    "YAPP-DEV-3.5.6-beta"
 #define VER_BUILD_DELIM     "-"
 
 time_t GetLatestTimestamp(void);

tags
@@ -441,6 +441,7 @@ g_pcIsTimeGood	src/yapp_fold.c	/^char *g_pcIsTimeGood = NULL;$/;"	v
 g_pcIsTimeGood	src/yapp_makerfimask.c	/^char *g_pcIsTimeGood = NULL;$/;"	v
 g_pcIsTimeGood	src/yapp_stat.c	/^char *g_pcIsTimeGood = NULL;$/;"	v
 g_pcIsTimeGood	src/yapp_viewdata.c	/^char *g_pcIsTimeGood = NULL;$/;"	v
+g_pcVersion	src/yapp_version.c	/^const char *g_pcVersion = "YAPP-DEV-3.5.6-beta-20141218-150644";$/;"	v
 g_pdDM	src/yapp_dedisplaw.c	/^double *g_pdDM = NULL;$/;"	v
 g_pdDelayTab	src/yapp_dedisplaw.c	/^double *g_pdDelayTab = NULL;$/;"	v
 g_pdMaxDelayTab	src/yapp_dedisplaw.c	/^double *g_pdMaxDelayTab = NULL;$/;"	v