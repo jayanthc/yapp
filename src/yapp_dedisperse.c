jayanthc/yapp

commit 1b3c5c1ad399dbbf0d55870607fcfb66c0d57df5

Pulse markers for viewdata, bug fixes and minor edits.

src/yapp_dedisperse.c
@@ -86,11 +86,9 @@ int main(int argc, char *argv[])
     float fSampSize = 0.0;      /* number of bits that make a sample */
     int iTotSampsPerBlock = 0;  /* iNumChans * iBlockSize */
     int iDataSizePerBlock = 0;  /* fSampSize * iNumChans * iBlockSize */
-    int iNumGoodChans = 0;
     int iEffcNumGoodChans = 0;
     float fStatBW = 0.0;
     float fNoiseRMS = 0.0;
-    double dNumSigmas = 0.0;
     double dTNextBF = 0.0;
     double dTSamp = 0.0;        /* holds sampling time in ms */
     double dTSampInSec = 0.0;   /* holds sampling time in s */
@@ -368,7 +366,6 @@ int main(int argc, char *argv[])
     dTSampInSec = stYUM.dTSamp / 1e3;
     fChanBW = stYUM.fChanBW;
     iTimeSamps = stYUM.iTimeSamps; 
-    iNumGoodChans = stYUM.iNumGoodChans;
     fSampSize = stYUM.fSampSize;
     lDataSizeTotal = stYUM.lDataSizeTotal;
     pfTimeSectGain = stYUM.pfBFGain;    /* for .spec */
@@ -564,19 +561,6 @@ int main(int argc, char *argv[])
                   iNumReads,
                   iBlockSize);
 
-    /* calculate the threshold */
-    dNumSigmas = YAPP_CalcThresholdInSigmas(iTimeSampsToProc);
-    if ((double) YAPP_RET_ERROR == dNumSigmas)
-    {
-        (void) fprintf(stderr, "ERROR: Threshold calculation failed!\n");
-        YAPP_CleanUp();
-        return YAPP_RET_ERROR;
-    }
-    fStatBW = iNumGoodChans * fChanBW;  /* in MHz */
-    (void) printf("Usable bandwidth                  : %g MHz\n", fStatBW);
-    fNoiseRMS = 1.0 / sqrt(fStatBW * dTSamp * 1e3);
-    (void) printf("Expected noise RMS                : %g\n", fNoiseRMS);
-
     /* allocate memory for the time sample goodness flag array */
     g_pcIsTimeGood = (char *) YAPP_Malloc((size_t) iTimeSampsToProc,
                                           sizeof(char),
@@ -592,27 +576,38 @@ int main(int argc, char *argv[])
     /* set all elements to 'YAPP_TRUE' */
     (void) memset(g_pcIsTimeGood, YAPP_TRUE, iTimeSampsToProc);
 
+    /* add header for .fil or .tim file format */
+    if ((YAPP_FORMAT_FIL == iOutputFormat)
+        || (YAPP_FORMAT_DTS_TIM == iOutputFormat))
+    {
+        /* populate the YUM structure */
+        (void) memcpy(&stYUMOut, &stYUM, sizeof(YUM_t));
+    }
+
     /* kludge: simulate sub-band dedispersion by marking other channels as bad
        so that they don't go into the summation */
-    /* NOTE: this will not work with data in which there are actual bad
-       channels */
     if (iNumSubBands > 0)
     {
-        iChansPerSubBand = (int) ((float) iNumChans / iNumSubBands);
+        /* compute the number of channels per sub-band. iNumChans is a multiple
+           of iNumSubBands */
+        iChansPerSubBand = iNumChans / iNumSubBands;
         iStartChan = iSubBand * iChansPerSubBand;
         iEndChan = (iSubBand + 1) * iChansPerSubBand;
+        stYUMOut.iNumGoodChans = iChansPerSubBand;
         for (i = 0; i < iNumChans; ++i)
         {
-            if ((i >= iStartChan) && (i < iEndChan))
-            {
-                stYUM.pcIsChanGood[i] = YAPP_TRUE;
-            }
-            else
+            if (!((i >= iStartChan) && (i < iEndChan)))
             {
                 stYUM.pcIsChanGood[i] = YAPP_FALSE;
+                --stYUMOut.iNumGoodChans;
             }
         }
     }
+
+    fStatBW = stYUMOut.iNumGoodChans * fChanBW;  /* in MHz */
+    (void) printf("Usable bandwidth                  : %g MHz\n", fStatBW);
+    fNoiseRMS = 1.0 / sqrt(fStatBW * dTSamp * 1e3);
+    (void) printf("Expected noise RMS                : %g\n", fNoiseRMS);
 
     /* open the data file for reading */
     g_pFData = fopen(pcFileSpec, "r");
@@ -895,12 +890,10 @@ int main(int argc, char *argv[])
         (void) strcat(acFileDedisp, EXT_FIL);
     }
 
-    /* add header for .fil or .tim file format */
+    /* update header for .fil or .tim file format */
     if ((YAPP_FORMAT_FIL == iOutputFormat)
         || (YAPP_FORMAT_DTS_TIM == iOutputFormat))
     {
-        /* populate the YUM structure */
-        (void) memcpy(&stYUMOut, &stYUM, sizeof(YUM_t)); 
         stYUMOut.iNumBits = YAPP_SAMPSIZE_32;
         /* enter the start time corrected for dispersion */
         stYUMOut.dTStart = stYUM.dTStart - (fStartOffset / 86400);
@@ -915,7 +908,6 @@ int main(int argc, char *argv[])
         if (iNumSubBands > 0)
         {
             stYUMOut.iNumChans = iChansPerSubBand;
-            stYUMOut.iNumGoodChans = iChansPerSubBand;
             stYUMOut.fBW = stYUMOut.iNumChans * stYUMOut.fChanBW;
             stYUMOut.fFMin = stYUM.fFMin
                              + (iStartChan * stYUMOut.fChanBW);

src/yapp_makever.c
@@ -24,7 +24,7 @@
 /* TODO: read the path as argument */
 #define FILE_VERSRC         "src/yapp_version.c"
 #define VAR_VER             "*g_pcVersion"
-#define VER_BUILD_PREFIX    "YAPP-REL-3.4.6-beta"
+#define VER_BUILD_PREFIX    "YAPP-REL-3.4.7-beta"
 #define VER_BUILD_DELIM     "-"
 
 time_t GetLatestTimestamp(void);

src/yapp_add.c
@@ -199,7 +199,7 @@ int main(int argc, char *argv[])
     /* read metadata */
     /* NOTE: it is assumed that all files correspond to a single observation.
              the only information taken from all files except the last one is
-             the highest frequency. general metadata is read from the last
+             the highest frequency. common metadata is read from the last
              file */
     /* allocate memory for the frequency array */
     pafMaxFreq = (float *) YAPP_Malloc((size_t) iNumBands,

src/yapp_common.c
@@ -24,7 +24,7 @@ const char g_aacSP_ObsNames[YAPP_SP_NUMOBS][LEN_GENSTRING] = {
     YAPP_SP_OBS_EFFELSBERG,
 };
 
-extern const double g_aadErfLookup[YAPP_ERF_ENTRIES][3];
+extern const double g_aadErfLookup[YAPP_ERF_ENTRIES][2];
 
 /* global memory management */
 void* g_apvMemTable[YAPP_MAX_MEMTABLE] = {0};
@@ -193,7 +193,6 @@ double YAPP_CalcThresholdInSigmas(int iTimeSamps)
     double dErf = 0.0;
     double dNumSigmas = 0.0;
     double dErfRef = 0.0;
-    double dErfArgRef = 0.0;
     double dNumSigmasRef = 0.0;
     double dErfDiff = 0.0;
     double dErfDiffMin = 0.0;
@@ -208,7 +207,6 @@ double YAPP_CalcThresholdInSigmas(int iTimeSamps)
     dErf = 1 - 2 * dPOutlier;
 
     dErfRef = g_aadErfLookup[0][0];
-    dErfArgRef = g_aadErfLookup[0][1];
     dNumSigmasRef = g_aadErfLookup[0][2];
     dErfDiff = fabs(dErfRef - dErf);
     dErfDiffMin = dErfDiff;
@@ -219,7 +217,6 @@ double YAPP_CalcThresholdInSigmas(int iTimeSamps)
     for (i = 0; i < YAPP_ERF_ENTRIES; ++i)
     {
         dErfRef = g_aadErfLookup[i][0];
-        dErfArgRef = g_aadErfLookup[i][1];
         dNumSigmasRef = g_aadErfLookup[i][2];
 
         dErfDiff = fabs(dErfRef - dErf);

src/yapp_ft.c
@@ -1201,53 +1201,53 @@ void PrintUsage(const char *pcProgName)
 {
     (void) printf("Usage: %s [options] <data-file>\n",
                   pcProgName);
-    (void) printf("    -h  --help                           ");
+    (void) printf("    -h  --help                          ");
     (void) printf("Display this usage information\n");
-    (void) printf("    -s  --skip <time>                    ");
+    (void) printf("    -s  --skip <time>                   ");
     (void) printf("The length of data in seconds, to be\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("skipped\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("(default is 0 s)\n");
-    (void) printf("    -p  --proc <time>                    ");
+    (void) printf("    -p  --proc <time>                   ");
     (void) printf("The length of data in seconds, to be\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("processed\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("(default is all)\n");
-    (void) printf("    -l  --tsamp <tsamp>                  ");
+    (void) printf("    -l  --tsamp <tsamp>                 ");
     (void) printf("Sampling time in s\n");
-    (void) printf("    -f  --centre-freq <freq>             ");
+    (void) printf("    -f  --centre-freq <freq>            ");
     (void) printf("Centre frequency of observing band,\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("in MHz\n");
-    (void) printf("    -b  --pfb                            ");
+    (void) printf("    -b  --pfb                           ");
     (void) printf("Do polyphase filter bank\n");
-    (void) printf("    -t  --ntaps <ntaps>                  ");
+    (void) printf("    -t  --ntaps <ntaps>                 ");
     (void) printf("Number of taps in the PFB\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("(default is 8)\n");
-    (void) printf("    -n  --nfft <nfft>                    ");
+    (void) printf("    -n  --nfft <nfft>                   ");
     (void) printf("Number of points in FFT\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("(default is 1024)\n");
-    (void) printf("    -a  --nacc <acc-len>                 ");
+    (void) printf("    -a  --nacc <acc-len>                ");
     (void) printf("Number of spectra to accumulate\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("(default is 1)\n");
-    (void) printf("    -o  --obs-site                       ");
+    (void) printf("    -o  --obs-site                      ");
     (void) printf("Observatory name\n");
-    (void) printf("    -r  --pulsar                         ");
+    (void) printf("    -r  --pulsar                        ");
     (void) printf("Pulsar name\n");
-    (void) printf("    -g  --graphics                       ");
+    (void) printf("    -g  --graphics                      ");
     (void) printf("Turn on plotting\n");
-    (void) printf("    -i  --invert                         ");
+    (void) printf("    -i  --invert                        ");
     (void) printf("Invert background and foreground\n");
-    (void) printf("                                         ");
+    (void) printf("                                        ");
     (void) printf("colours in plots\n");
-    (void) printf("    -e  --non-interactive                ");
+    (void) printf("    -e  --non-interactive               ");
     (void) printf("Run in non-interactive mode\n");
-    (void) printf("    -v  --version                        ");
+    (void) printf("    -v  --version                       ");
     (void) printf("Display the version\n");
 
     return;

src/yapp_viewdata.c
@@ -15,6 +15,9 @@
  *                                          (default is 4096 samples)
  *     -c  --clip-level <level>             Number of sigmas above threshold;
  *                                          will clip anything above this level
+ *     -t  --period <period>                Period of the pulsar in ms
+ *     -r  --phase <phase>                  Phase of the pulse with respect to
+ *                                          the first sample
  *     -m  --colour-map <name>              Colour map for plotting
  *                                          (default is 'jet')
  *     -i  --invert                         Invert the background and foreground
@@ -99,6 +102,11 @@ int main(int argc, char *argv[])
     float fButY = 0.0;
     char cCurChar = 0;
     int iNumSamps = 0;
+    double dPeriod = 0.0;
+    double dStartPhase = 0.0;
+    double dPhase = 0.0;
+    double dMinPhaseDiff = 0.0;
+    long int lSampCount = 0;
     int iDiff = 0;
     int i = 0;
     int j = 0;
@@ -112,7 +120,7 @@ int main(int argc, char *argv[])
     const char *pcProgName = NULL;
     int iNextOpt = 0;
     /* valid short options */
-    const char* const pcOptsShort = "hs:p:n:c:m:iev";
+    const char* const pcOptsShort = "hs:p:n:c:t:r:m:iev";
     /* valid long options */
     const struct option stOptsLong[] = {
         { "help",                   0, NULL, 'h' },
@@ -120,6 +128,8 @@ int main(int argc, char *argv[])
         { "proc",                   1, NULL, 'p' },
         { "nsamp",                  1, NULL, 'n' },
         { "clip-level",             1, NULL, 'c' },
+        { "period",                 1, NULL, 't' },
+        { "phase",                  1, NULL, 'r' },
         { "colour-map",             1, NULL, 'm' },
         { "invert",                 0, NULL, 'i' },
         { "non-interactive",        0, NULL, 'e' },
@@ -167,6 +177,16 @@ int main(int argc, char *argv[])
             case 'c':   /* -c or --clip-level */
                 /* set option */
                 fClipLevel = (float) atof(optarg);
+                break;
+
+            case 't':   /* -t or --period */
+                /* set option */
+                dPeriod = atof(optarg);
+                break;
+
+            case 'r':   /* -r or --phase */
+                /* set option */
+                dStartPhase = atof(optarg);
                 break;
 
             case 'm':   /* -m or --colour-map */
@@ -420,7 +440,6 @@ int main(int argc, char *argv[])
 
     if ((YAPP_FORMAT_FIL == iFormat) || (YAPP_FORMAT_DTS_TIM == iFormat))
     {
-        /* TODO: Need to do this only if the file contains the header */
         /* skip the header */
         (void) fseek(g_pFData, (long) stYUM.iHeaderLen, SEEK_SET);
         /* skip data, if any are to be skipped */
@@ -535,6 +554,12 @@ int main(int argc, char *argv[])
                        strerror(errno));
         YAPP_CleanUp();
         return YAPP_RET_ERROR;
+    }
+
+    if (dPeriod != 0.0)
+    {
+        /* compute the phase difference between successive samples */
+        dMinPhaseDiff = (stYUM.dTSamp / dPeriod);
     }
 
     while (iNumReads > 0)
@@ -787,6 +812,23 @@ int main(int argc, char *argv[])
             cpgsci(PG_CI_DEF);
         }
 
+        if (dPeriod != 0.0)
+        {
+            cpgsci(7);
+            for (i = 0; i < iNumSamps; ++i)
+            {
+                /* compute the phase */
+                dPhase = (double) lSampCount * (stYUM.dTSamp / dPeriod);
+                dPhase = dPhase - floor(dPhase);
+                if (fabs(dPhase - dStartPhase) < dMinPhaseDiff)
+                {
+                    cpgerry(1, &g_pfXAxis[i], &fDataMax, &fDataMin, 0.0);
+                }
+                ++lSampCount;
+            }
+            cpgsci(PG_CI_DEF);
+        }
+
         /* display the plot number */
         (void) sprintf(acLabel, "%d / %d", iReadBlockCount, iTotNumReads);
         cpgmtxt("T", -1.5, 0.99, 1.0, acLabel);
@@ -909,6 +951,12 @@ void PrintUsage(const char *pcProgName)
     (void) printf("Number of sigmas above threshold; will\n");
     (void) printf("                                        ");
     (void) printf("clip anything above this level\n");
+    (void) printf("    -t  --period <period>               ");
+    (void) printf("Period of the pulsar in ms\n");
+    (void) printf("    -r  --phase <phase>                 ");
+    (void) printf("Phase of the pulse with respect to the\n");
+    (void) printf("                                        ");
+    (void) printf("first sample\n");
     (void) printf("    -m  --colour-map <name>             ");
     (void) printf("Colour map for plotting\n");
     (void) printf("                                        ");

commit 1336e47e597c13e9060fe7c6df2a49da9b622627

Fixed subband dedispersion bug introduced in 1b3c5c1.

src/yapp_dedisperse.c
@@ -648,7 +648,6 @@ int main(int argc, char *argv[])
             if (!((i >= iStartChan) && (i < iEndChan)))
             {
                 stYUM.pcIsChanGood[i] = YAPP_FALSE;
-                --stYUMOut.iNumGoodChans;
             }
         }
     }

src/yapp_makever.c
@@ -24,7 +24,7 @@
 /* TODO: read the path as argument */
 #define FILE_VERSRC         "src/yapp_version.c"
 #define VAR_VER             "*g_pcVersion"
-#define VER_BUILD_PREFIX    "YAPP-REL-3.5.5-beta"
+#define VER_BUILD_PREFIX    "YAPP-REL-3.5.6-beta"
 #define VER_BUILD_DELIM     "-"
 
 time_t GetLatestTimestamp(void);